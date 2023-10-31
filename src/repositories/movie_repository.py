37e78a862b5a0c52f096c81b6bfdd11f130e313b
ekkanikanti <143454787+ekkanikanti@users.noreply.github.com>
2023-10-31 18:15:37 -0400

    Update movie_repository.py

--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -2,19 +2,19 @@ class MovieRepository:
 
     def get_all_movies(self):
         # TODO get all movies from the DB
-        return None
+        return self.query.all()
 
     def get_movie_by_id(self, movie_id):
         # TODO get a single movie from the DB using the ID
-        return None
+        return self.query.get(movie_id)
 
     def create_movie(self, title, director, rating):
         # TODO create a new movie in the DB
-        return None
+        return self.session.add(title, director, rating)
 
     def search_movies(self, title):
         # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
-        return None
+        return self.query.filter(self.message.match(title)).all()
 
 
 # Singleton to be used in other modules
