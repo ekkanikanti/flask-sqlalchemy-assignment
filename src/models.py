42b1308efbdc5dac87222fa313fa142ea954d891
ekkanikanti <143454787+ekkanikanti@users.noreply.github.com>
2023-10-31 18:15:11 -0400

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,1 +1,9 @@
 # TODO - Create SQLAlchemy DB and Movie model
+from sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class movie(db.Model):
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    director = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Integer, nullable=True)
